7fec137f17ae6c8c770047664fb9511f9fb28d2a
JustGod <85418813+JustGodWork@users.noreply.github.com>
2024-01-24 15:35:26 +0100

    Delete python-if_else_loops_functions/8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            char = chr(ord(char) - 32)
-        print(char, end="")
-    print()
