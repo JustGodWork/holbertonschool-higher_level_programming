234d2e302cfdd92e6ded35b0161875b2be571207
JustGod <85418813+JustGodWork@users.noreply.github.com>
2024-01-24 15:35:11 +0100

    Delete python-if_else_loops_functions/6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i < j:
-            print("{}{}".format(i, j), end="")
-            if i < 8:
-                print(", ", end="")
