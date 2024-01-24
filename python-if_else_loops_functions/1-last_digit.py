45c985f248d182c7cce6423c77c4b96217742563
JustGodWork <discord1elie@gmail.com>
2024-01-24 15:42:48 +0100

    Fixed conflit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+print("Last digit of", number, "is", last_digit, end=" ")
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = number % 10
+    print(last_digit)
+    return last_digit
