990639cfe554d04fe2181e48f6f396778aaeac54
JustGodWork <discord1elie@gmail.com>
2024-02-02 09:52:49 +0100

    Updated safe print list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-        print()
-        return i + 1
     except IndexError:
+        x = i
+    finally:
         print()
-        return i
+        return x
