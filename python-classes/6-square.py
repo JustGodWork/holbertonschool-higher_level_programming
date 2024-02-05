4d50c3b37fad9eb8e616af31979d34d7cd4b8baf
JustGodWork <discord1elie@gmail.com>
2024-02-05 18:28:48 +0100

    Updated task 6 from project 1

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,8 +5,17 @@
 class Square:
     """A class to represent a square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+        if not isinstance(position, tuple) or len(position) != 2 or \
+           not all(isinstance(i, int) and i >= 0 for i in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
 
     @property
     def size(self):
