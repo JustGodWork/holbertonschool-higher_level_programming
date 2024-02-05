8ff3ecec4404cc99b2194abfde980871a19b8645
JustGodWork <discord1elie@gmail.com>
2024-02-05 18:47:06 +0100

    Testing task 1 project 2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,4 +8,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    if (type(a) is float):
+        a = int(a)
+    if (type(b) is float):
+        b = int(b)
+    return a + b
