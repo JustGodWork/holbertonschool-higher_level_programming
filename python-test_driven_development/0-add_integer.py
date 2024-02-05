28c766286d11b51b829689f037777def856b8fcf
JustGodWork <discord1elie@gmail.com>
2024-02-05 18:44:21 +0100

    Testing task 1 project 2

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module that add two integers."""
+
+
+def add_integer(a, b=98):
+    """Adds two integers."""
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
