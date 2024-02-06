0a16b3b61c1f5dcc1c5468d67f5bf74a632e99f0
JustGodWork <discord1elie@gmail.com>
2024-02-06 10:23:45 +0100

    Fix task 5 project 2

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,13 +15,18 @@ def text_indentation(text):
     Raises:
         TypeError: If text is not a string.
     """
-    if type(text) is not str:
+    if type(text) is not (str):
         raise TypeError("text must be a string")
 
-    text = text.strip()
-    new_text = ""
-    for char in text:
-        new_text += char
-        if char in ".?:":
-            new_text += "\n\n"
-    print(new_text, end="")
+    sentence = ""
+
+    for letter in text:
+
+        sentence += letter
+
+        if letter in [".", "?", ":"]:
+            print("{}\n".format(sentence.strip()))
+            sentence = ""
+
+    if sentence.strip() != "":
+        print("{}".format(sentence.strip()), end="")
