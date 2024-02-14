c677d57d35c22c852fcddae453b29b4a876c427f
JustGodWork <discord1elie@gmail.com>
2024-02-14 09:52:22 +0100

    Fixed task 7 (project 3)

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-This module contains a script that adds all arguments to a Python list,
-and then save them to a file.
-"""
-
-
+"""Add and save all arguments to a Python list"""
 import sys
-
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 args = sys.argv[1:]
 filename = "add_item.json"
-
-try:
-    my_list = load_from_json_file(filename)
+with open(filename, "a+") as f:
+    try:
+        my_list = load_from_json_file(filename)
+    except Exception:
+        my_list = []
     my_list.extend(args)
-except (FileNotFoundError, AttributeError):
-    my_list = []
-
-save_to_json_file(my_list, filename)
+    save_to_json_file(my_list, filename)
