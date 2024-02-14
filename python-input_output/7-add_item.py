65a220f02665302854dbc438535a10c5465a8b4b
JustGodWork <discord1elie@gmail.com>
2024-02-14 09:39:28 +0100

    Added AttributeError listener

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,16 +9,13 @@ import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 args = sys.argv[1:]
-
 filename = "add_item.json"
 
 try:
     my_list = load_from_json_file(filename)
-except FileNotFoundError:
+    my_list.extend(args)
+except (FileNotFoundError, AttributeError):
     my_list = []
-
-my_list.extend(args)
 
 save_to_json_file(my_list, filename)
