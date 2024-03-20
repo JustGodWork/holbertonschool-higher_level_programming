1e37fab92fb2626d78a9476756c36b5fc579972a
JustGodWork <discord1elie@gmail.com>
2024-03-20 10:19:46 +0100

    Added task 5 of project 'SQL Introduction'

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- Lists all columns from table named first_table in your database.
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+-- Print description from table named first_table in your database.
+ SHOW CREATE TABLE `first_table`;
