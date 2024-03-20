0f1d6dff058cf25db9a3c1083dee5f381ddc27f5
JustGodWork <discord1elie@gmail.com>
2024-03-20 09:54:21 +0100

    Added task 4 of project 'SQL Introduction'

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Create a table named first_table if not exists with one column id of type INT and a column name of type VARCHAR(256).
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
